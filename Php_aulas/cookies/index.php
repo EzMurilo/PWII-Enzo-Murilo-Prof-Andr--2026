f98697836ee6526e0e572360a9ec4bcfa0165529
EzMurilo <enzomurilo321@gmail.com>
2026-09-18 22:12:18 -0300

    Bd e php

---
+++ Php_aulas/cookies/index.php
@@ -0,0 +1,17 @@
+<?php
+include "valida_cookies.inc"
+?>
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validação de Cookies</title>
+</head>
+<body>
+    Seja bem-vindo ao meu site!!!<br>
+    Coloque o conteudo do site aqui neste espaço<br>
+    <p><a href="logout.php"></a></p>
+</body>
+</html>
